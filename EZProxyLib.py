e9fb5a73e6f76a2a36f016c06ca004fad77ea91d
Pure | Cody <43043067+PurityWasHere@users.noreply.github.com>
2020-06-08 17:48:23 -0700

    Update EZProxyLib.py

--- EZProxyLib.py
+++ EZProxyLib.py
@@ -1,14 +1,3 @@
-'''
-EZProxyLib is a Easy to Use Proxy loading lib for easily loading Http/Https Proxies, Aswell as Socks4/Socks5 Proxies.
-
-How To Use
----------------
-Step 1) Put this file in the same folder as your project, or load it as a library
-Step 2) Call the function you want ex: GrabProxiesHTTP('proxies.txt') < If the proxies.txt file is in your local folder.
-Step 3) Call RefreshProxy() Every time you want a new proxy to be used. Make sure to do step 2 first to propigate the proxies list.
-Thats all. Have Fun having easy access to proxies while keeping your code clean.
-'''
-
 import random
 proxyCount = 0
 proxies = []
